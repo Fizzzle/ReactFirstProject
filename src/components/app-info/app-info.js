2516c312824833dc3b16e13005c112f2f4d30c91
Viktor <Rickeygame23@gmail.com>
2022-07-21 12:38:50 +0300

    e

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет татумастеров в компании KinkyTattoo</h1>
+            <h2>Общее число татумастеров : </h2>
+            <h2>Премии получат : </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input
+            className="form-control search-input"
+            placeholder="Найти татумастера"
+            type="text" />
+    )
+}
+
+export default SearchPanel;
